fe1df1bf2091344bda88f01b8fc9b3b85676aba9
bing <bingxuan.liu@cern.ch>
2015-07-29 20:03:43 -0400

    more modulized

--- plugin/TriggerAnalyzerModule.cc
+++ plugin/TriggerAnalyzerModule.cc
@@ -4,21 +4,34 @@ using namespace std;
 using namespace reco;
 using namespace edm;
 
-//An analyzer to get the efficiency w.r.t reco quantities. 
+//An analyzer to get the hlt/reco turn on curves. wrt gen
 TriggerAnalyzerModule::TriggerAnalyzerModule (const edm::ParameterSet &cfg) :
   beamSpot_ (cfg.getParameter<edm::InputTag> ("beamSpot")),
   recoMuon_ (cfg.getParameter<edm::InputTag> ("recoMuons")),
+  recoTrack_ (cfg.getParameter<edm::InputTag> ("recoTracks")),
   triggerSummaryLabel_ (cfg.getParameter<edm::InputTag> ("triggerSummaryLabel")),
   triggerResultsLabel_ (cfg.getParameter<edm::InputTag> ("triggerResultsLabel")),
   filterTag_ (cfg.getParameter<string> ("filterTag")),
   ptCut_ (cfg.getParameter<double> ("ptCut")),
+  histograms_ (cfg.getParameter<vector<edm::ParameterSet>>("histograms_")),
   l1FilterName_ (cfg.getParameter<string> ("l1FilterName")),
-  l3FilterName_ (cfg.getParameter<string> ("l3FilterName"))
+  l2FilterName_ (cfg.getParameter<string> ("l2FilterName")),
+  iDTag_ (cfg.getParameter<string> ("iDTag")),
+  muonStudy_ (cfg.getParameter<bool> ("muonStudy")),
+  trackStudy_ (cfg.getParameter<bool> ("trackStudy")),
+  l1OnRaw_ (cfg.getParameter<bool> ("l1OnRaw")),
+  l2Onl1_ (cfg.getParameter<bool> ("l2Onl1"))
  {
-  TH1D::SetDefaultSumw2 ();
-  double vbins[17] = {0,20,25,30,35,36,37,38,39,40,41,42,45,50,60,100,200};
-  muonPt = fs->make<TH1D>("muonPt","muonPt",16,vbins);
-  muonMatchedPt = fs->make<TH1D>("muonMatchedPt","muonMatchedPt",16,vbins);
+  TH1D::SetDefaultSumw2 ();  
+  for(uint i =0; i < histograms_.size(); i++)
+    {
+      TH1D* tmpHist =  fs->make<TH1D>(TString(histograms_[i].getParameter<string>("variable")),TString(histograms_[i].getParameter<string>("variable")),int(histograms_[i].getParameter<int>("nBins")),histograms_[i].getParameter<double>("lBin"),histograms_[i].getParameter<double>("hBin"));     
+      vanilaHistograms.push_back(tmpHist);  
+      TH1D* tmpMatchedHist = fs->make<TH1D>(TString(histograms_[i].getParameter<string>("variable") + "_matched"),TString(histograms_[i].getParameter<string>("variable") + "_matched"),int(histograms_[i].getParameter<int>("nBins")),histograms_[i].getParameter<double>("lBin"),histograms_[i].getParameter<double>("hBin"));
+      matchedHistograms.push_back(tmpMatchedHist);  
+      TH1D* tmpEffHist = fs->make<TH1D>(TString(histograms_[i].getParameter<string>("variable") + "_eff"),TString(histograms_[i].getParameter<string>("variable") + "_eff"),int(histograms_[i].getParameter<int>("nBins")),histograms_[i].getParameter<double>("lBin"),histograms_[i].getParameter<double>("hBin"));
+      effHistograms.push_back(tmpEffHist);  
+    } 
  }
 
 TriggerAnalyzerModule::~TriggerAnalyzerModule ()
@@ -28,22 +41,31 @@ TriggerAnalyzerModule::~TriggerAnalyzerModule ()
 
 void
 TriggerAnalyzerModule::produce(edm::Event &event, const edm::EventSetup &setup)
-{
-  event.getByLabel(beamSpot_, BeamSpot);
-  event.getByLabel(triggerSummaryLabel_, triggerSummary);
-  event.getByLabel(triggerResultsLabel_, triggerResults);
-  trigger::TriggerObjectCollection allTriggerObjects = triggerSummary->getObjects();
-  string l3FilterName = l3FilterName_;
-  edm::InputTag l3FilterTag = edm::InputTag(l3FilterName, "", filterTag_);
-  size_t l3FilterIndex = (*triggerSummary).filterIndex(l3FilterTag);
-  trigger::TriggerObjectCollection l3Objects;
-  if(l3FilterIndex < (*triggerSummary).sizeFilters())
+{  
+  if(event.getByLabel(beamSpot_, BeamSpot))
     {
-       const trigger::Keys &keysObjects = (*triggerSummary).filterKeys(l3FilterIndex);
+      refx_ = BeamSpot->x0();
+      refy_ = BeamSpot->y0();
+    }
+  else
+   {
+     refx_ = 0;
+     refy_ = 0;
+   }
+  event.getByLabel(triggerSummaryLabel_, triggerSummary);
+  event.getByLabel(triggerResultsLabel_, triggerResults); 
+  trigger::TriggerObjectCollection allTriggerObjects = triggerSummary->getObjects(); 
+  string l2FilterName = l2FilterName_;
+  edm::InputTag l2FilterTag = edm::InputTag(l2FilterName, "", filterTag_);
+  size_t l2FilterIndex = (*triggerSummary).filterIndex(l2FilterTag);
+  trigger::TriggerObjectCollection l2Objects;
+  if(l2FilterIndex < (*triggerSummary).sizeFilters())
+    { 
+       const trigger::Keys &keysObjects = (*triggerSummary).filterKeys(l2FilterIndex);
        for(size_t j = 0; j < keysObjects.size(); j++)
          {
            trigger::TriggerObject foundObject = (allTriggerObjects)[keysObjects[j]];
-           l3Objects.push_back(foundObject);
+           l2Objects.push_back(foundObject);
          }
     }
   string l1FilterName = l1FilterName_;
@@ -51,7 +73,7 @@ TriggerAnalyzerModule::produce(edm::Event &event, const edm::EventSetup &setup)
   size_t l1FilterIndex = (*triggerSummary).filterIndex(l1FilterTag);
   trigger::TriggerObjectCollection l1Objects;
   if(l1FilterIndex < (*triggerSummary).sizeFilters())
-    {
+    { 
        const trigger::Keys &keysObjects = (*triggerSummary).filterKeys(l1FilterIndex);
        for(size_t j = 0; j < keysObjects.size(); j++)
          {
@@ -59,24 +81,69 @@ TriggerAnalyzerModule::produce(edm::Event &event, const edm::EventSetup &setup)
            l1Objects.push_back(foundObject);
          }
     }
-  vector<reco::Muon> slimmedRecoMuon = recoMuonSelector(recoMuon_, event);
-  vector<reco::Muon> l1MatchedRecoMuon = recoMuonLooper(l1Objects, slimmedRecoMuon);
-  vector<reco::Muon> l3MatchedRecoMuon = recoMuonLooper(l3Objects, l1MatchedRecoMuon);
-  fillHistogram(l1MatchedRecoMuon, muonPt);
-  fillHistogram(l3MatchedRecoMuon, muonMatchedPt);
-  slimmedRecoMuon.clear();
-  l1MatchedRecoMuon.clear();
-  l3MatchedRecoMuon.clear();
+  if(muonStudy_)
+    {
+      vector<reco::Muon> slimmedRecoMuon = recoMuonSelector(recoMuon_, event);
+      vector<reco::Muon> l1MatchedRecoMuon = recoMuonLooper(l1Objects, slimmedRecoMuon);
+      vector<reco::Muon> l2MatchedRecoMuon = recoMuonLooper(l2Objects, l1MatchedRecoMuon); 
+      if(l1OnRaw_)
+        {
+          for(uint j = 0; j < vanilaHistograms.size(); j++)
+            {
+              fillMuonHistogram(slimmedRecoMuon, vanilaHistograms[j]);    
+              fillMuonHistogram(l1MatchedRecoMuon, matchedHistograms[j]);
+              fillMuonHistogram(l1MatchedRecoMuon, effHistograms[j]);
+            }
+        }  
+      if(l2Onl1_)
+        {
+          for(uint j = 0; j < vanilaHistograms.size(); j++)
+            {
+              fillMuonHistogram(l1MatchedRecoMuon, vanilaHistograms[j]);    
+              fillMuonHistogram(l2MatchedRecoMuon, matchedHistograms[j]);
+              fillMuonHistogram(l2MatchedRecoMuon, effHistograms[j]);
+            }
+         }
+       l2MatchedRecoMuon.clear();
+       l1MatchedRecoMuon.clear();
+       slimmedRecoMuon.clear();
+    }
+  if(trackStudy_)
+    {
+      vector<reco::Track> slimmedRecoTrack = recoTrackSelector(recoTrack_, event);
+      vector<reco::Track> l1MatchedRecoTrack = recoTrackLooper(l1Objects, slimmedRecoTrack);
+      vector<reco::Track> l2MatchedRecoTrack = recoTrackLooper(l2Objects, l1MatchedRecoTrack); 
+      if(l1OnRaw_)
+        {
+          for(uint j = 0; j < vanilaHistograms.size(); j++)
+            {
+              fillTrackHistogram(slimmedRecoTrack, vanilaHistograms[j]);    
+              fillTrackHistogram(l1MatchedRecoTrack, matchedHistograms[j]);
+              fillTrackHistogram(l1MatchedRecoTrack, effHistograms[j]);
+            }
+        }  
+      if(l2Onl1_)
+        {
+          for(uint j = 0; j < vanilaHistograms.size(); j++)
+            {
+              fillTrackHistogram(l1MatchedRecoTrack, vanilaHistograms[j]);    
+              fillTrackHistogram(l2MatchedRecoTrack, matchedHistograms[j]);
+              fillTrackHistogram(l2MatchedRecoTrack, effHistograms[j]);
+            }
+         }
+       l2MatchedRecoTrack.clear();
+       l1MatchedRecoTrack.clear();
+       slimmedRecoTrack.clear();
+     }
 }
-
-double
+double 
 TriggerAnalyzerModule::dxyBeamSpot(double vx, double px, double vy, double py, double pt, double refx, double refy)
 {
   return (-(vx-refx)*py + (vy-refy)*px)/pt;
 }
 
 
-bool
+bool 
 TriggerAnalyzerModule::match(double eta_1, double phi_1, double eta_2, double phi_2)
 {
   bool matched = deltaR(eta_1,phi_1,eta_2,phi_2) < 0.1 ? true:false;
@@ -85,31 +152,79 @@ TriggerAnalyzerModule::match(double eta_1, double phi_1, double eta_2, double ph
 
 
 void
-TriggerAnalyzerModule::fillHistogram(vector<reco::Muon> recoMuonSet, TH1D* histogram)
+TriggerAnalyzerModule::fillMuonHistogram(vector<reco::Muon> recoMuonSet, TH1D* histogram)
 {
   for (unsigned int j = 0 ; j < recoMuonSet.size() ; j++)
     {
-       histogram->Fill(recoMuonSet[j].pt());
+       vector<string> name = getStringSegments(string(histogram->GetName()), '_');
+       double value = muonVariables(recoMuonSet[j], name[0]);
+       histogram->Fill(value);
     }
 }
-vector<reco::Muon>
+
+void
+TriggerAnalyzerModule::fillTrackHistogram(vector<reco::Track> recoTrackSet, TH1D* histogram)
+{
+  for (unsigned int j = 0 ; j < recoTrackSet.size() ; j++)
+    {
+       vector<string> name = getStringSegments(string(histogram->GetName()), '_');
+       double value = trackVariables(recoTrackSet[j], name[0]);
+       histogram->Fill(value);
+    }
+}
+
+double
+TriggerAnalyzerModule::trackVariables(const reco::Track track, string variable)
+{
+  double value = 0;
+  if(variable == "Pt") value = track.pt(); 
+  if(variable == "Eta") value = track.eta(); 
+  if(variable == "Phi") value = track.phi(); 
+  if(variable == "Dxy") value = dxyBeamSpot(track.vx(), track.px(), track.vy(), track.py(), track.pt(), refx_, refy_); 
+  return value;
+}
+
+double
+TriggerAnalyzerModule::muonVariables(const reco::Muon muon, string variable)
+{
+  double value = 0;
+  if(variable == "Pt") value = muon.pt(); 
+  if(variable == "Eta") value = muon.eta(); 
+  if(variable == "Phi") value = muon.phi(); 
+  if(variable == "Dxy") value = dxyBeamSpot(muon.vx(), muon.px(), muon.vy(), muon.py(), muon.pt(), refx_, refy_); 
+  return value;
+}
+
+vector<reco::Muon> 
 TriggerAnalyzerModule::recoMuonSelector(const edm::InputTag recoMuonTag, edm::Event &event)
 {
   vector<reco::Muon> selectedRecoMuon;
   edm::Handle<reco::MuonCollection> MuonCollection;
   if(event.getByLabel(recoMuonTag, MuonCollection))
     {
-      for (reco::MuonCollection::const_iterator recoMuon = MuonCollection ->begin(); recoMuon != MuonCollection->end(); recoMuon++)
+      for (reco::MuonCollection::const_iterator recoMuon = MuonCollection ->begin(); recoMuon != MuonCollection->end(); recoMuon++) 
         {
           reco::Muon thisParticle = (*recoMuon);
-          bool tightId = recoMuon->isGlobalMuon() &&
-                         recoMuon->globalTrack()->normalizedChi2() < 10 &&
-                         recoMuon->globalTrack()->hitPattern().numberOfValidMuonHits() > 0 &&
-                         recoMuon->numberOfMatchedStations() > 1 &&
-                         recoMuon->innerTrack()->hitPattern().numberOfValidPixelHits() > 0 &&
-                         recoMuon->innerTrack()->hitPattern().trackerLayersWithMeasurement() > 5 &&
-                         recoMuon->pt() > ptCut_ && fabs(recoMuon->eta()) < 2.4 && recoMuon->isPFMuon();
-          if(tightId)
+          bool tight = recoMuon->isGlobalMuon() && 
+                       recoMuon->globalTrack()->normalizedChi2() < 10 && 
+                       recoMuon->globalTrack()->hitPattern().numberOfValidMuonHits() > 0 && 
+                       recoMuon->numberOfMatchedStations() > 1 &&
+                       recoMuon->innerTrack()->hitPattern().numberOfValidPixelHits() > 0 &&
+                       recoMuon->innerTrack()->hitPattern().trackerLayersWithMeasurement() > 5 &&
+                       recoMuon->pt() > ptCut_ && fabs(recoMuon->eta()) < 2.4 && recoMuon->isPFMuon(); 
+          //bool goodGlob = recoMuon->isGlobalMuon() && 
+          //                recoMuon->globalTrack()->normalizedChi2() < 3; 
+          //                recoMuon->combinedQuality().chi2LocalPosition < 12 && 
+          //                recoMuon->combinedQuality().trkKink < 20;
+          //bool medium =  recoMuon->innerTrack()->validFraction() > 0.8 &&
+          //               muon::segmentCompatibility(thisParticle) > (goodGlob ? 0.303 : 0.451) &&
+          //               muon::isLooseMuon(thisParticle) &&
+          //               recoMuon->pt() > ptCut_ && fabs(recoMuon->eta()) < 2.4 &&  goodGlob;
+          //bool loose = muon::isLooseMuon(thisParticle);
+          //             recoMuon->isGlobalMuon() && recoMuon->isPFMuon();
+          //bool passId = tagToBool(iDTag_, loose, medium, tight);
+          //if(passId)
+          if(tight)
             {
               selectedRecoMuon.push_back(thisParticle);
             }
@@ -119,33 +234,115 @@ TriggerAnalyzerModule::recoMuonSelector(const edm::InputTag recoMuonTag, edm::Ev
   selectedRecoMuon.clear();
 }
 
+vector<reco::Track> 
+TriggerAnalyzerModule::recoTrackSelector(const edm::InputTag recoTrackTag, edm::Event &event)
+{
+  vector<reco::Track> selectedRecoTrack;
+  edm::Handle<reco::TrackCollection> TrackCollection;
+  if(event.getByLabel(recoTrackTag, TrackCollection))
+    {
+      for (reco::TrackCollection::const_iterator recoTrack = TrackCollection ->begin(); recoTrack != TrackCollection->end(); recoTrack++) 
+        {
+          reco::Track thisParticle = (*recoTrack);
+          bool tight =  recoTrack->normalizedChi2() < 10 && 
+                                recoTrack->hitPattern().numberOfValidMuonHits() > 0 && 
+                                recoTrack->hitPattern().muonStationsWithValidHits() > 1 &&
+                                recoTrack->hitPattern().numberOfValidPixelHits() > 0 &&
+                                recoTrack->hitPattern().trackerLayersWithMeasurement() > 5 &&
+                                recoTrack->pt() > ptCut_ && fabs(recoTrack->eta()) < 2.4;
+          bool medium = recoTrack->normalizedChi2() < 10 && 
+                                recoTrack->hitPattern().numberOfValidMuonHits() > 0 && 
+                                recoTrack->hitPattern().muonStationsWithValidHits() > 1 &&
+                                recoTrack->hitPattern().trackerLayersWithMeasurement() > 3 &&
+                                recoTrack->pt() > ptCut_ && fabs(recoTrack->eta()) < 2.4;
+          bool loose = recoTrack->normalizedChi2() < 10 && 
+                                recoTrack->hitPattern().numberOfValidMuonHits() > 0 && 
+                                recoTrack->hitPattern().muonStationsWithValidHits() > 1 &&
+                                recoTrack->pt() > ptCut_ && fabs(recoTrack->eta()) < 2.4;
+          bool passId = tagToBool(iDTag_, loose, medium, tight);  
+          if(passId)
+            {
+              selectedRecoTrack.push_back(thisParticle);
+            }
+          }
+       }
+  return selectedRecoTrack;
+  selectedRecoTrack.clear();
+}
+
 vector<reco::Muon>
 TriggerAnalyzerModule::recoMuonLooper(const trigger::TriggerObjectCollection Objects, vector<reco::Muon> recoMuonSet)
 {
   vector<reco::Muon> matchedRecoMuon;
-  if(recoMuonSet.size())
+  if(recoMuonSet.size()) 
     {
       for (unsigned int j = 0 ; j < recoMuonSet.size() ; j++)
         {
           reco::Muon thisParticle = recoMuonSet[j];
           for(unsigned int i = 0 ; i < Objects.size() ; i++)
-            {
+            {                 
               if(match(thisParticle.eta(),thisParticle.phi(),Objects[i].eta(),Objects[i].phi()))
                 {
                   matchedRecoMuon.push_back(thisParticle);
                 }
-            }
+            }  
          }
      }
   return matchedRecoMuon;
   matchedRecoMuon.clear();
 }
 
+vector<reco::Track>
+TriggerAnalyzerModule::recoTrackLooper(const trigger::TriggerObjectCollection Objects, vector<reco::Track> recoTrackSet)
+{
+  vector<reco::Track> matchedRecoTrack;
+  if(recoTrackSet.size()) 
+    {
+      for (unsigned int j = 0 ; j < recoTrackSet.size() ; j++)
+        {
+          reco::Track thisParticle = recoTrackSet[j];
+          for(unsigned int i = 0 ; i < Objects.size() ; i++)
+            {                 
+              if(match(thisParticle.eta(),thisParticle.phi(),Objects[i].eta(),Objects[i].phi()))
+                {
+                  matchedRecoTrack.push_back(thisParticle);
+                }
+            }  
+         }
+     }
+  return matchedRecoTrack;
+  matchedRecoTrack.clear();
+}
+
+bool
+TriggerAnalyzerModule::tagToBool(const string tag, bool loose, bool medium, bool tight)
+{
+ bool iD = false;
+ if(tag == "loose") iD = loose;
+ if(tag == "medium") iD = medium;
+ if(tag == "tight") iD = tight;
+ return iD;
+}
+
+vector<string>
+TriggerAnalyzerModule::getStringSegments(string input, char delim)
+{
+    vector<string> elems;
+    std::stringstream ss(input);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
 void
 TriggerAnalyzerModule::endJob()
 {
- muonMatchedPt->Divide(muonMatchedPt, muonPt,1,1,"B") ;
+  for(uint m = 0; m < effHistograms.size(); m++)
+    {
+      effHistograms[m]->Divide(effHistograms[m], vanilaHistograms[m],1,1,"B"); 
+    }
 }
-
 DEFINE_FWK_MODULE(TriggerAnalyzerModule);
-~                                                     
--- plugin/TriggerAnalyzerModule.h
+++ plugin/TriggerAnalyzerModule.h
@@ -58,12 +58,19 @@ class TriggerAnalyzerModule : public edm::EDProducer
       //void analyze (const edm::Event &, const edm::EventSetup &);
       bool match(double eta_1, double phi_1, double eta_2, double phi_2);
       double dxyBeamSpot(double vx, double px, double vy, double py, double pt, double refx, double refy);
+      bool tagToBool(const string tag, bool loose, bool medium, bool tight);
       vector<reco::Muon> recoMuonSelector(const edm::InputTag recoMuonTag, edm::Event &event);
       vector<reco::Muon> recoMuonLooper(const trigger::TriggerObjectCollection Objects, vector<reco::Muon> recoMuonSet);
-      void fillHistogram(vector<reco::Muon> recoMuonSet, TH1D* histogram);
+      vector<reco::Track> recoTrackSelector(const edm::InputTag recoTrackTag, edm::Event &event);
+      vector<reco::Track> recoTrackLooper(const trigger::TriggerObjectCollection Objects, vector<reco::Track> recoTrackSet);
+      void fillMuonHistogram(vector<reco::Muon> recoMuonSet, TH1D* histogram);
+      void fillTrackHistogram(vector<reco::Track> recoTrackSet, TH1D* histogram);
+      vector<string> getStringSegments(string input, char delim);
+      double trackVariables(const reco::Track track, string variable);
+      double muonVariables(const reco::Muon muon, string variable);
       TriggerAnalyzerModule (const edm::ParameterSet &);
       ~TriggerAnalyzerModule();
-      virtual void endJob() override;
+      virtual void endJob() override; 
 
       edm::Service<TFileService> fs;
     private:
@@ -71,15 +78,25 @@ class TriggerAnalyzerModule : public edm::EDProducer
       edm::Handle<trigger::TriggerEvent> triggerSummary;
       edm::InputTag beamSpot_;
       edm::InputTag recoMuon_;
+      edm::InputTag recoTrack_;
       edm::InputTag triggerSummaryLabel_;
       edm::InputTag triggerResultsLabel_;
       string filterTag_;
       double ptCut_;
-      edm::Handle<edm::TriggerResults> triggerResults;
+      vector<edm::ParameterSet> histograms_;
+      edm::Handle<edm::TriggerResults> triggerResults; 
       string l1FilterName_;
-      string l3FilterName_;
-      TH1D* muonPt;
-      TH1D* muonMatchedPt;
-};
+      string l2FilterName_;
+      string iDTag_;
+      bool muonStudy_;
+      bool trackStudy_;
+      bool l1OnRaw_;
+      bool l2Onl1_;
+      vector<TH1D*> vanilaHistograms;
+      vector<TH1D*> matchedHistograms;
+      vector<TH1D*> effHistograms;
+      double refx_;
+      double refy_;
+};  
 
 //#endif
